ae147f45771352e17f21a01822a9b2b182d09b8f
Brandon Williams <bwilliams.eng@gmail.com>
2022-11-21 11:15:28 -0800

    connection: remove outdated comment

--- crates/anemo/src/connection.rs
+++ crates/anemo/src/connection.rs
@@ -87,9 +87,6 @@ impl Connection {
     /// Open a unidirection stream to the peer.
     ///
     /// Messages sent over the stream will arrive at the peer in the order they were sent.
-    //TODO Today when a SendStream is dropped, it still attempts to re-transmit any data that was
-    // previously enqueued. This may be non-ideal for dealing with things like timeouts and we may
-    // want to look at explicitly calling Reset on the stream if it is dropped pre-maturely.
     #[allow(dead_code)]
     pub async fn open_uni(&self) -> Result<SendStream, ConnectionError> {
         self.inner.open_uni().await.map(SendStream)
