a5aaa18c8a67edb626f34ffe837b80283390247b
George Danezis <george@danez.is>
2022-11-21 15:22:38 +0000

    Implement drop for send_stream in peer.

--- crates/anemo/src/network/peer.rs
+++ crates/anemo/src/network/peer.rs
@@ -5,8 +5,22 @@ use super::{
 use crate::{connection::Connection, PeerId, Request, Response, Result};
 use bytes::Bytes;
 use futures::future::BoxFuture;
-use tokio_util::codec::{FramedRead, FramedWrite};
+use quinn::{RecvStream, SendStream};
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 use tower::{Layer, Service, ServiceExt};
+
+// A private structure to hold streams and ensure that if it is dropped
+// the send side is reset to free retransmit buffers.
+struct StreamPair {
+    pub send_stream: FramedWrite<SendStream, LengthDelimitedCodec>,
+    pub recv_stream: FramedRead<RecvStream, LengthDelimitedCodec>,
+}
+
+impl Drop for StreamPair {
+    fn drop(&mut self) {
+        let _ = self.send_stream.get_mut().reset(0u8.into());
+    }
+}
 
 /// Handle to a connection with a remote Peer.
 #[derive(Clone)]
@@ -36,21 +50,26 @@ impl Peer {
 
     async fn do_rpc(&self, request: Request<Bytes>) -> Result<Response<Bytes>> {
         let (send_stream, recv_stream) = self.connection.open_bi().await?;
-        let mut send_stream = FramedWrite::new(send_stream, network_message_frame_codec());
-        let mut recv_stream = FramedRead::new(recv_stream, network_message_frame_codec());
+        let send_stream = FramedWrite::new(send_stream, network_message_frame_codec());
+        let recv_stream = FramedRead::new(recv_stream, network_message_frame_codec());
+
+        let mut stream_pair = StreamPair {
+            send_stream,
+            recv_stream,
+        };
 
         //
         // Write Request
         //
 
-        write_request(&mut send_stream, request).await?;
-        send_stream.get_mut().finish().await?;
+        write_request(&mut stream_pair.send_stream, request).await?;
+        stream_pair.send_stream.get_mut().finish().await?;
 
         //
         // Read Response
         //
 
-        let mut response = read_response(&mut recv_stream).await?;
+        let mut response = read_response(&mut stream_pair.recv_stream).await?;
 
         // Set the PeerId of this peer
         response.extensions_mut().insert(self.peer_id());
