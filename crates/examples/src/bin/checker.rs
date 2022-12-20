f1038835fa49c910d5cf53ae03bc8eb67a30c107
Brandon Williams <bwilliams.eng@gmail.com>
2022-12-20 14:35:40 -0800

    checker

---
+++ crates/examples/src/bin/checker.rs
@@ -0,0 +1,27 @@
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let network = anemo::Network::bind("0.0.0.0:0")
+        .private_key(random_key())
+        .server_name("sui")
+        .start(anemo::Router::new())
+        .unwrap();
+    info!("address: {:?}", network.local_addr());
+
+    let result = network
+        .connect("validator-0.devnet.sui.io:8084")
+        // .connect("fullnode.devnet.sui.io:8084")
+        // .connect("43.201.94.190:8082")
+        // .connect("sea-suival-0.testnet.sui.io:8082")
+        .await;
+    info!("{:?}", result);
+}
+
+fn random_key() -> [u8; 32] {
+    let mut rng = rand::thread_rng();
+    let mut bytes = [0u8; 32];
+    rand::RngCore::fill_bytes(&mut rng, &mut bytes[..]);
+    bytes
+}
