2a0fe4630324960fe0b805ec814be92ae8306431
Andrew Schran <aschran@mystenlabs.com>
2023-01-09 10:11:05 -0800

    Add option to inflight limit layer for error response when limit exceeded.

--- crates/anemo-tower/src/inflight_limit.rs
+++ crates/anemo-tower/src/inflight_limit.rs
@@ -3,7 +3,7 @@
 //! # Example
 //!
 //! ```
-//! use anemo_tower::inflight_limit::InflightLimitLayer;
+//! use anemo_tower::inflight_limit::{InflightLimitLayer, WaitMode};
 //! use anemo::{Request, Response};
 //! use tower::{Service, ServiceExt, ServiceBuilder, service_fn};
 //! use bytes::Bytes;
@@ -15,7 +15,7 @@
 //! # #[tokio::main]
 //! # async fn main() -> Result<(), anemo::rpc::Status> {
 //! let mut service = ServiceBuilder::new()
-//!     .layer(InflightLimitLayer::new(3))
+//!     .layer(InflightLimitLayer::new(3, WaitMode::ReturnError))
 //!     .service_fn(handle);
 //!
 //! // Set fake PeerId.
@@ -31,7 +31,7 @@
 //! # }
 //! ```
 
-use anemo::{PeerId, Request, Response};
+use anemo::{types::response::StatusCode, PeerId, Request, Response};
 use dashmap::DashMap;
 use futures::future::BoxFuture;
 use std::{
@@ -42,6 +42,15 @@ use tokio::sync::Semaphore;
 use tower::{layer::Layer, Service};
 use tracing::debug;
 
+/// What to do if inflight limit is exceeded.
+#[derive(Clone, Copy, Debug)]
+pub enum WaitMode {
+    /// Blocks request until it can be serviced.
+    Block,
+    /// Returns an error indicating the minimum wait time required to try again.
+    ReturnError,
+}
+
 /// [`Layer`] for adding a per-peer inflight limit to inbound requests.
 ///
 /// See the [module docs](super::inflight_limit) for more details.
@@ -49,14 +58,16 @@ use tracing::debug;
 pub struct InflightLimitLayer {
     inflight: Arc<DashMap<PeerId, Arc<Semaphore>>>,
     max_inflight: usize,
+    wait_mode: WaitMode,
 }
 
 impl InflightLimitLayer {
     /// Create a new [`InflightLimitLayer`].
-    pub fn new(max_inflight: usize) -> Self {
+    pub fn new(max_inflight: usize, wait_mode: WaitMode) -> Self {
         InflightLimitLayer {
             inflight: Arc::new(DashMap::new()),
             max_inflight,
+            wait_mode,
         }
     }
 }
@@ -69,6 +80,7 @@ impl<S> Layer<S> for InflightLimitLayer {
             inner,
             inflight: self.inflight.clone(),
             max_inflight: self.max_inflight,
+            wait_mode: self.wait_mode,
         }
     }
 }
@@ -82,15 +94,17 @@ pub struct InflightLimit<S> {
     // TODO: Garbage collect old PeerIds.
     inflight: Arc<DashMap<PeerId, Arc<Semaphore>>>,
     max_inflight: usize,
+    wait_mode: WaitMode,
 }
 
 impl<S> InflightLimit<S> {
     /// Create a new [`InflightLimit`].
-    pub fn new(inner: S, max_inflight: usize) -> Self {
+    pub fn new(inner: S, max_inflight: usize, wait_mode: WaitMode) -> Self {
         Self {
             inner,
             inflight: Arc::new(DashMap::new()),
             max_inflight,
+            wait_mode,
         }
     }
 
@@ -112,10 +126,11 @@ impl<S> InflightLimit<S> {
     /// Returns a new [`Layer`] that wraps services with a `InflightLimit` middleware.
     ///
     /// [`Layer`]: tower::layer::Layer
-    pub fn layer(max_inflight: usize) -> InflightLimitLayer {
+    pub fn layer(max_inflight: usize, wait_mode: WaitMode) -> InflightLimitLayer {
         InflightLimitLayer {
             inflight: Arc::new(DashMap::new()),
             max_inflight,
+            wait_mode,
         }
     }
 }
@@ -141,6 +156,7 @@ where
     fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
         let inflight = self.inflight.clone();
         let max_inflight = self.max_inflight;
+        let wait_mode = self.wait_mode;
         let mut inner = self.inner.clone();
 
         let fut = async move {
@@ -153,11 +169,21 @@ where
                     .or_insert_with(|| Arc::new(Semaphore::new(max_inflight)));
                 semaphore_entry.value().clone()
             };
-            let _permit = semaphore.acquire().await.map_err(|e| {
-                anemo::rpc::Status::internal(format!(
-                    "failed to acquire inflight limiter permit: {e:?}"
-                ))
-            })?;
+            let _permit = match wait_mode {
+                WaitMode::Block => semaphore.acquire().await.map_err(|e| {
+                    anemo::rpc::Status::internal(format!(
+                        "failed to acquire inflight limiter permit: {e:?}"
+                    ))
+                })?,
+                WaitMode::ReturnError => semaphore.try_acquire().map_err(|e| match e {
+                    tokio::sync::TryAcquireError::Closed => {
+                        anemo::rpc::Status::new(StatusCode::InternalServerError)
+                    }
+                    tokio::sync::TryAcquireError::NoPermits => {
+                        anemo::rpc::Status::new(StatusCode::TooManyRequests)
+                    }
+                })?,
+            };
             debug!("acquired inflight limiter permit for peer {peer_id:?}");
             inner.call(req).await
         };
@@ -167,14 +193,14 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::InflightLimitLayer;
-    use anemo::{Request, Response};
+    use super::{InflightLimitLayer, WaitMode};
+    use anemo::{types::response::StatusCode, Request, Response};
     use bytes::Bytes;
     use std::time::Duration;
     use tower::{service_fn, ServiceBuilder, ServiceExt};
 
     #[tokio::test]
-    async fn basic() {
+    async fn block() {
         let working_service = service_fn(|_req: Request<Bytes>| async move {
             Ok::<_, anemo::rpc::Status>(Response::new(Bytes::new()))
         });
@@ -187,13 +213,13 @@ mod tests {
 
         // Verify we get a result for working service.
         let svc = ServiceBuilder::new()
-            .layer(InflightLimitLayer::new(1))
+            .layer(InflightLimitLayer::new(1, WaitMode::Block))
             .service(working_service);
         let request = Request::new(Bytes::new()).with_extension(peer);
         svc.oneshot(request).await.unwrap();
 
         // Verify waiting service blocks.
-        let layer = InflightLimitLayer::new(1);
+        let layer = InflightLimitLayer::new(1, WaitMode::Block);
         let working_svc = ServiceBuilder::new()
             .layer(layer.clone())
             .service(working_service);
@@ -208,5 +234,35 @@ mod tests {
         let timeout_resp =
             tokio::time::timeout(Duration::from_secs(2), working_svc.oneshot(request)).await;
         assert!(timeout_resp.is_err()); // working request should be blocked behind waiting request
+    }
+
+    #[tokio::test]
+    async fn return_error() {
+        let working_service = service_fn(|_req: Request<Bytes>| async move {
+            Ok::<_, anemo::rpc::Status>(Response::new(Bytes::new()))
+        });
+        let waiting_service = service_fn(|_req: Request<Bytes>| async move {
+            tokio::time::sleep(Duration::from_secs(86400)).await;
+            Ok::<_, anemo::rpc::Status>(Response::new(Bytes::new()))
+        });
+
+        let peer = anemo::PeerId([0; 32]);
+
+        // Verify waiting service returns error.
+        let layer = InflightLimitLayer::new(1, WaitMode::ReturnError);
+        let working_svc = ServiceBuilder::new()
+            .layer(layer.clone())
+            .service(working_service);
+        let waiting_svc = ServiceBuilder::new()
+            .layer(layer.clone())
+            .service(waiting_service);
+
+        let request = Request::new(Bytes::new()).with_extension(peer);
+        tokio::task::spawn(waiting_svc.oneshot(request));
+        tokio::time::sleep(Duration::from_millis(100)).await; // let waiting service start
+
+        let request = Request::new(Bytes::new()).with_extension(peer);
+        let response = working_svc.oneshot(request).await;
+        assert_eq!(response.unwrap_err().status(), StatusCode::TooManyRequests);
     }
 }
